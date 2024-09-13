8296c82ef56f94ed497d022a2f97e1ecf004fd1f
Vedant62 <vedant.sharma0510@gmail.com>
2024-09-14 03:00:39 +0530

    added settings dropdown options for project

--- lib/pages/individual_project.dart
+++ lib/pages/individual_project.dart
@@ -13,6 +13,7 @@ class IndividualProject extends StatefulWidget {
 
 class _IndividualProjectState extends State<IndividualProject> {
   late Project _project;
+
   @override
   void initState() {
     // TODO: implement initState
@@ -23,7 +24,47 @@ class _IndividualProjectState extends State<IndividualProject> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(_project.title),),
+      appBar: AppBar(
+        title: Text(_project.title),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: DropdownButtonHideUnderline(
+
+              child: DropdownButton<String>(
+
+                borderRadius: BorderRadius.circular(8),
+                icon: const Icon(Icons.settings_rounded),
+                items: <DropdownMenuItem<String>>[
+                  DropdownMenuItem<String>(
+                    onTap: (){print('lmao');},
+                    value: 'edit',
+                    child: Text(
+                      'Edit',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                          fontSize: 20, fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                  DropdownMenuItem<String>(
+                    onTap: (){print('lmao2');},
+                    value: 'delete',
+                    child: Text(
+                      'Delete',
+                      style: Theme.of(context).textTheme.labelLarge!.copyWith(
+                          color: Theme.of(context).colorScheme.error,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w300),
+                    ),
+                  ),
+                ],
+                onChanged: (String? value) {
+                  // Handle the selected option here
+                },
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
